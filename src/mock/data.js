6c05d333eaf2793d6e84cb47c6b600537157a80c
zhangzhongming <zhangzhongming@bytedance.com>
2023-01-30 19:31:24 +0800

    feat: update resume

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,128 +1,129 @@
-import VARIABLE from './zh_CN.js';
+import VARIABLE from "./zh_CN.js";
 
 export const baseInfo = {
-    name: VARIABLE.NAME,
-    list: [
-        {
-            label: VARIABLE.GENDER_LABEL,
-            value: VARIABLE.GENDER_VALUE
-        },
-        {
-            label: VARIABLE.BIRTHDAY_LABEL,
-            value: VARIABLE.BIRTHDAY_VALUE
-        },
-        {
-            label: VARIABLE.NATIVE_PLACE_LABEL,
-            value: VARIABLE.NATIVE_PLACE_VALUE
-        },
-        {
-            label: VARIABLE.PHONE_LABEL,
-            value: VARIABLE.PHONE_VALUE
-        },
-        {
-            label: VARIABLE.EMAIL_LABEL,
-            value: VARIABLE.EMAIL_VALUE
-        },
-        {
-            label: VARIABLE.PERSONAL_BLOG_LABEL,
-            value: VARIABLE.PERSONAL_BLOG_VALUE,
-            link: true
-        },
-        {
-            label: VARIABLE.EXPERIENCE_LABEL,
-            value: VARIABLE.EXPERIENCE_VALUE
-        },
-        {
-            label: VARIABLE.INTENTION_LABEL,
-            value: VARIABLE.INTENTION_VALUE
-        },
-    ]
-}
+  name: VARIABLE.NAME,
+  list: [
+    {
+      label: VARIABLE.GENDER_LABEL,
+      value: VARIABLE.GENDER_VALUE,
+    },
+    {
+      label: VARIABLE.BIRTHDAY_LABEL,
+      value: VARIABLE.BIRTHDAY_VALUE,
+    },
+    {
+      label: VARIABLE.NATIVE_PLACE_LABEL,
+      value: VARIABLE.NATIVE_PLACE_VALUE,
+    },
+    {
+      label: VARIABLE.PHONE_LABEL,
+      value: VARIABLE.PHONE_VALUE,
+    },
+    {
+      label: VARIABLE.EMAIL_LABEL,
+      value: VARIABLE.EMAIL_VALUE,
+    },
+    {
+      label: VARIABLE.PERSONAL_BLOG_LABEL,
+      value: VARIABLE.PERSONAL_BLOG_VALUE,
+      link: true,
+    },
+    {
+      label: VARIABLE.EXPERIENCE_LABEL,
+      value: VARIABLE.EXPERIENCE_VALUE,
+    },
+    {
+      label: VARIABLE.INTENTION_LABEL,
+      value: VARIABLE.INTENTION_VALUE,
+    },
+  ],
+};
 
 export const education_module = {
-    title: VARIABLE.TITLE_EDUCATION,
-    list: [
-        {
-            label: VARIABLE.SCHOOL_LABEL,
-            value: VARIABLE.SCHOOL_VALUE
-        },
-        {
-            label: VARIABLE.EDUCATION_LABEL,
-            value: VARIABLE.EDUCATION_VALUE,
-        },
-        {
-            label: VARIABLE.MAJOR_LABEL,
-            value: VARIABLE.MAJOR_VALUE,
-        }
-    ]
-}
+  title: VARIABLE.TITLE_EDUCATION,
+  list: [
+    {
+      label: VARIABLE.SCHOOL_LABEL,
+      value: VARIABLE.SCHOOL_VALUE,
+    },
+    {
+      label: VARIABLE.EDUCATION_LABEL,
+      value: VARIABLE.EDUCATION_VALUE,
+    },
+    {
+      label: VARIABLE.MAJOR_LABEL,
+      value: VARIABLE.MAJOR_VALUE,
+    },
+  ],
+};
 
 export const skill_module = {
-    title: VARIABLE.TITLE_SKILL,
-    list: [
-        VARIABLE.SKILL_1,
-        VARIABLE.SKILL_2,
-        VARIABLE.SKILL_3,
-        VARIABLE.SKILL_4,
-        VARIABLE.SKILL_5,
-        VARIABLE.SKILL_6,
-        VARIABLE.SKILL_7,
-    ]
-}
+  title: VARIABLE.TITLE_SKILL,
+  list: [
+    VARIABLE.SKILL_1,
+    VARIABLE.SKILL_2,
+    VARIABLE.SKILL_3,
+    VARIABLE.SKILL_4,
+    VARIABLE.SKILL_5,
+    VARIABLE.SKILL_6,
+    VARIABLE.SKILL_7,
+  ],
+};
 
 export const job_module = {
-    title: VARIABLE.TITLE_JOB,
-    list: [
-        {
-            times: VARIABLE.JOB_1_TIMES,
-            companyName: VARIABLE.JOB_1_COMPANY_NAME,
-            jobName: VARIABLE.JOB_1_JOB_NAME,
-            subtitle: VARIABLE.SUBTITLE_JOB,
-            contentList: [
-                VARIABLE.JOB_1_CONTENT_1,
-                VARIABLE.JOB_1_CONTENT_2,
-                VARIABLE.JOB_1_CONTENT_3,
-                VARIABLE.JOB_1_CONTENT_4,
-            ]
-        },
-        {
-            times: VARIABLE.JOB_2_TIMES,
-            companyName: VARIABLE.JOB_2_COMPANY_NAME,
-            jobName: VARIABLE.JOB_2_JOB_NAME,
-            subtitle: VARIABLE.SUBTITLE_JOB,
-            contentList: [
-                VARIABLE.JOB_2_CONTENT_1,
-                VARIABLE.JOB_2_CONTENT_2,
-                VARIABLE.JOB_2_CONTENT_3,
-                VARIABLE.JOB_2_CONTENT_4,
-            ]
-        },
-        {
-            times: VARIABLE.JOB_3_TIMES,
-            companyName: VARIABLE.JOB_3_COMPANY_NAME,
-            jobName: VARIABLE.JOB_3_JOB_NAME,
-            subtitle: VARIABLE.SUBTITLE_JOB,
-            contentList: [
-                VARIABLE.JOB_3_CONTENT_1,
-                VARIABLE.JOB_3_CONTENT_2,
-                VARIABLE.JOB_3_CONTENT_3,
-                VARIABLE.JOB_3_CONTENT_4,
-            ]
-        },
-        {
-            times: VARIABLE.JOB_4_TIMES,
-            companyName: VARIABLE.JOB_4_COMPANY_NAME,
-            jobName: VARIABLE.JOB_4_JOB_NAME,
-            subtitle: VARIABLE.SUBTITLE_JOB,
-            contentList: [
-                VARIABLE.JOB_4_CONTENT_1,
-                VARIABLE.JOB_4_CONTENT_2,
-            ]
-        }
-    ]
-}
+  title: VARIABLE.TITLE_JOB,
+  list: [
+    {
+      times: VARIABLE.JOB_1_TIMES,
+      companyName: VARIABLE.JOB_1_COMPANY_NAME,
+      jobName: VARIABLE.JOB_1_JOB_NAME,
+      subtitle: VARIABLE.SUBTITLE_JOB,
+      contentList: [
+        VARIABLE.JOB_1_CONTENT_1,
+        VARIABLE.JOB_1_CONTENT_2,
+        VARIABLE.JOB_1_CONTENT_3,
+        VARIABLE.JOB_1_CONTENT_4,
+      ],
+    },
+    {
+      times: VARIABLE.JOB_2_TIMES,
+      companyName: VARIABLE.JOB_2_COMPANY_NAME,
+      jobName: VARIABLE.JOB_2_JOB_NAME,
+      subtitle: VARIABLE.SUBTITLE_JOB,
+      contentList: [
+        VARIABLE.JOB_2_CONTENT_1,
+        VARIABLE.JOB_2_CONTENT_2,
+        VARIABLE.JOB_2_CONTENT_3,
+        VARIABLE.JOB_2_CONTENT_4,
+      ],
+    },
+    {
+      times: VARIABLE.JOB_3_TIMES,
+      companyName: VARIABLE.JOB_3_COMPANY_NAME,
+      jobName: VARIABLE.JOB_3_JOB_NAME,
+      subtitle: VARIABLE.SUBTITLE_JOB,
+      contentList: [
+        VARIABLE.JOB_3_CONTENT_1,
+        VARIABLE.JOB_3_CONTENT_2,
+        VARIABLE.JOB_3_CONTENT_3,
+        VARIABLE.JOB_3_CONTENT_4,
+      ],
+    },
+    {
+      times: VARIABLE.JOB_4_TIMES,
+      companyName: VARIABLE.JOB_4_COMPANY_NAME,
+      jobName: VARIABLE.JOB_4_JOB_NAME,
+      subtitle: VARIABLE.SUBTITLE_JOB,
+      contentList: [
+        VARIABLE.JOB_4_CONTENT_1,
+        VARIABLE.JOB_4_CONTENT_2,
+        VARIABLE.JOB_4_CONTENT_3,
+      ],
+    },
+  ].reverse(),
+};
 
 export const evaluation_module = {
-    title: VARIABLE.TITLE_EVALUATION,
-    evaluationContent: VARIABLE.SLEF_EVALUATION
-}
+  title: VARIABLE.TITLE_EVALUATION,
+  evaluationContent: VARIABLE.SLEF_EVALUATION,
+};
